442eed5df044645a2f56e3bae95abf28b91408f9
Daviswww <35088642+Daviswww@users.noreply.github.com>
2018-08-09 19:04:30 +0800

    Add files via upload

---
+++ volume001/UVA147.cpp
@@ -0,0 +1,30 @@
+//https://uva.onlinejudge.org/external/1/147.pdf
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long a[6001];
+int b[]={1,2,4,10,20,40,100,200,400,1000,2000};
+int main(void)
+{
+	for (int i = 0; i <= 6000;++i) 
+	{
+		a[i]=1;
+	}
+	for (int i = 1; i < 11; ++i)
+	{
+		for (int j= b[i] ;j <= 6000; ++j)
+		{
+			a[j]+=a[j-b[i]];
+		}
+	}
+	while(true)
+	{
+		double d;
+		scanf("%lf", &d);
+		if(d == 0.0) break;
+		int n=int(d * 20.0);
+		printf("%6.2lf%17lld\n", d, a[n]);
+	}
+	return 0;
+}

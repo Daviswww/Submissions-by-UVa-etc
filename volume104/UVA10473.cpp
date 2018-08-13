f8a5da2b461a8aa2a7733d3a7613f356084422d1
Xing-Wei Ho <ho861206@gmail.com>
2018-08-13 18:10:32 +0800

    Add files via upload

---
+++ volume104/UVA10473.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include<iostream>
+#include<sstream>
+#include<cstdio>
+using namespace std;
+
+int main(){
+	string str;
+	stringstream ss;
+	int sum;
+	while( cin >> str )
+	{
+		if( str[0] == '-' )
+		{
+			break;
+		}
+		ss.str(str);
+		int a, b;
+		if( str[1] == 'x' )
+		{
+			ss >> hex >> sum;
+			printf( "%d\n", sum);
+		}
+		else
+		{
+			ss >> dec >> sum;
+			printf( "0x%X\n", sum);
+		}
+		ss.clear();
+	}
+  	return 0;
+}

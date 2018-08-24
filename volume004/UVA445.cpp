8f87c2992f97940668978e19809e123abf394e0d
Xing-Wei Ho <ho861206@gmail.com>
2018-08-24 10:12:47 +0800

    Add files via upload

---
+++ volume004/UVA445.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	
+    char c;
+    int n = 0;
+    while ( (c = getchar()) != EOF )
+	{
+        if (c == '\n' || c == '!')
+        {
+			cout << endl;
+		}
+        else if (c >= '0' && c <= '9')
+        {
+        	n += (c - '0');
+		}
+        else
+		{
+            if (c == 'b')
+			{
+				for (int i = 0; i < n ; i++)
+				{
+					cout << " ";
+				}
+			}
+            else
+			{
+				for (int i = 0; i < n ; i++)
+				{
+					cout << c;
+				}
+            }
+			n = 0;
+        }
+    }
+    return 0;
+}

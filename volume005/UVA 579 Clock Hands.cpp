23f283f4aa2d081371dbcb75af958622c2380391
Xing-Wei Ho <ho861206@gmail.com>
2018-08-28 16:32:00 +0800

    Update and rename UVA579.cpp to UVA 579 Clock Hands.cpp

--- volume005/UVA 579 Clock Hands.cpp
+++ volume005/UVA 579 Clock Hands.cpp
@@ -6,12 +6,12 @@ int main()
     int H,M;
     while (scanf("%d:%d",&H,&M) != EOF){
         if (!H && !M) break;
-        double H_minute = (double(H) + double(M)/60) * 5; // 將時針換成分鐘表示
+        double H_minute = (double(H) + double(M)/60) * 5;
         double M_minute = double(M);
         double diff = Abs(H_minute - M_minute);
         while (diff >= 60) diff -= 60;
         if (diff > 30) diff = 60 - diff; 
-        printf("%.3f\n", diff * 6); // 一分鐘6度
+        printf("%.3f\n", diff * 6);
     }
     return 0;
 }

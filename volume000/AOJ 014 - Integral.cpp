610857e8c0af70b8afcde809b6014b11c74b1fd4
Xing-Wei Ho <ho861206@gmail.com>
2018-09-09 00:47:24 +0800

    Add files via upload

---
+++ volume000/AOJ 014 - Integral.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int x);
+
+int main()
+{
+	int d;
+	
+	while( cin >> d )
+	{
+		int sum = 0;
+		for(int i=1 ; (d*i) <= (600-d) ; i++)
+		{
+			sum += f(d*i) * d;
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
+
+int f(int x)
+{
+	return (x*x);
+}

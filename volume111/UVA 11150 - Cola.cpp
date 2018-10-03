7337f17e54f26695e6c0cb1c7d7b3b9572114af3
Xing-Wei Ho <ho861206@gmail.com>
2018-10-03 19:59:54 +0800

    Add files via upload

---
+++ volume111/UVA 11150 - Cola.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	while(cin >> n)
+	{
+		int ans = n, cola;
+		while(n >= 3)
+		{
+			int t = n / 3; 
+			ans += t, n %= 3, n += t;
+		}
+		if(n == 2)
+		{
+			printf("%d\n", ans+1);
+		}
+		else
+		{
+			printf("%d\n", ans);
+		}
+	}
+	return 0;
+}
+ 

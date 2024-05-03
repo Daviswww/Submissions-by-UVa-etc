e219270c4efe7c48443a89af7f1792d5af3bd8b2
Davis Ho <ho861206@gmail.com>
2024-05-03 22:05:17 +0800

    Add LC 2095-1 - Delete the Middle Node of a Linked List

---
+++ Leetcode Medium/LC 2095-1 - Delete the Middle Node of a Linked List.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *deleteMiddle(struct ListNode *head)
+{
+    struct ListNode **indir = &head;
+    for (struct ListNode *fast = head; fast && fast->next; fast = fast->next->next)
+        indir = &(*indir)->next;
+    struct ListNode *del = *indir;
+    *indir = (*indir)->next;
+    free(del);
+    return head;
+}

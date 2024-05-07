5811a878ff9a4bef8f62ce5c4c90b10c1c3614d6
Davis Ho <ho861206@gmail.com>
2024-05-07 21:16:57 +0800

    LC 0141 - Linked List Cycle.c

---
+++ Leetcode Easy/LC 0141 - Linked List Cycle.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head)
+{
+    struct ListNode *fast = head;
+
+    while (head && fast && fast->next)
+    {
+        head = head->next;
+        fast = fast->next->next;
+
+        if (head == fast)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
